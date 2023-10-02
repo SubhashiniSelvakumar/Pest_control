7cc1e22a15df04819c737abe04f5fef4a71d336b
SubhashiniSelvakumar <146129745+SubhashiniSelvakumar@users.noreply.github.com>
2023-10-02 22:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,23 @@
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-from rich import print
+# from streamlit_lottie import st_lottie
+# from PIL import Image
+# import io
 
-
-# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-
+# def load_lottieurl(png):
+#     r = requests.get(url)
+#     if r.status_code != 200:
+#         return None
+#     return r.json()
+# lottie_coding = load_lottieurl("https://lottie.host/f3188099-228a-4cfe-bfbd-e3f5b88ed680/BpFet5AtOZ.json")
+# Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
-        css_content = f.read()
-        print("CSS Content:")
-        print(css_content)
-        st.markdown(f"<style>{css_content}</style>", unsafe_allow_html=True)
-
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 local_css("style.css")
 
-st.subheader("Hi, I am Khaja Mohideen :wave:")
+st.subheader("Hi, I am Khaja Mohideen:wave:")
 st.title("A PEST CONTROL SERVICE PROVIDER")
 st.write("""
 Im Khaja Mohideen, the founder and owner of ERASE Pest Control Services. 
@@ -36,10 +29,12 @@ with a focus on quality, safety, and eco-friendliness. We're here to help you en
 pest-free environment, whether it's in your home or business. Thank you for considering 
 ERASE Pest Control Services for your pest control needs.
 """)
-st.write("[Learn More >](https://pythonandvba.com)")
+
+#st.write("I'm Khaja Mohideen, the founder and owner of ERASE Pest Control Services. With a passion for pest management and a dedication to providing effective solutions, I've built a trusted name in the industry. With years of experience and a commitment to customer satisfaction, I lead a team of professionals who excel in tackling all your pest-related concerns. Our mission is to deliver top-notch pest control services with a focus on quality, safety, and eco-friendliness. We're here to help you enjoy a pest-free environment, whether it's in your home or business. Thank you for considering ERASE Pest Control Services for your pest control needs.")
+st.write("[Learn More >](https://instagram.com/erase_pest_no1?igshid=MmU2YjMzNjRlOQ==)")
 
 #loading assets
-lottie_coding =load_lottieurl("https://lottie.host/f3188099-228a-4cfe-bfbd-e3f5b88ed680/BpFet5AtOZ.json")
+# lottie_coding =load_lottieurl("https://lottie.host/f3188099-228a-4cfe-bfbd-e3f5b88ed680/BpFet5AtOZ.json")
 
 # ---- WHAT I DO ----
 with st.container():
@@ -50,18 +45,54 @@ with st.container():
         st.write("##")
         st.write(
             """
-            On my YouTube channel I am creating tutorials for people who:
-            - are looking for a way to leverage the power of Python in their day-to-day work.
-            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-            - are working with Excel and found themselves thinking - "there has to be a better way."
+            Residential Pest Control
 
-            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+1.**Elimination of pests in homes:**
+- Customized solutions for homeowners
+- Safe and effective treatments
+- Commercial Pest Management
+
+2.**Pest control services for businesses:**
+- Tailored solutions for commercial spaces
+- Minimizing disruptions to operations
+- Pest Identification
+
+3.**Expert pest diagnosis**
+- Identification of pest species
+- Accurate assessment of infestations
+- Pest Extermination
+
+4.**Swift and efficient pest removal**
+- Proven pest control methods
+- Safe and responsible extermination
+- Preventative Services
+
+5.**Prevention of future infestations**
+- Customized prevention plans
+- Ongoing maintenance and monitoring
+- Expertise and Experience
+
+6.**Experienced team of pest control professionals**
+- Certified technicians
+- In-depth knowledge of pest biology and behavior
+- Service Areas
+
+7.**Coverage areas and regions served by your business**
+- Customer-Centric Approach
+
+8.**Commitment to customer satisfaction**
+- Tailoring services to meet unique needs
+- Responsive customer support
+
+9.**Why Choose Us**
+- Years of industry experience
+- Emphasis on safety and eco-friendliness
+- Cost-effective pest control solutions
             """
         )
-        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
-    with right_column:
-        st_lottie(lottie_coding, height=300, key="coding")
+        st.write("[Instagram Profile >](https://instagram.com/erase_pest_no1?igshid=MmU2YjMzNjRlOQ==)")
+    # with right_column:
+    #     st_lottie(lottie_coding, height=300, key="coding")
 
 # ---- CONTACT ----
 with st.container():
@@ -71,7 +102,7 @@ with st.container():
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-    <form action="https://formsubmit.co/sathishmech1810@gmail.com" method="POST">
+    <form action="https://formsubmit.co/erasepestcontrolservices@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
